52afdcf211b4403cbdf22a970d31f664c84046b7
doossee <84768497+doossee@users.noreply.github.com>
2024-07-02 12:14:02 +0500

    some bug fix

--- src/management/urls.py
+++ src/management/urls.py
@@ -1,12 +1,21 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import *
+from .views import (
+    UserViewSet,
+    AdminViewSet,
+    DoctorViewSet,
+    PatientViewSet,
+    SpecialtyViewSet,
+    ServiceViewSet,
+    InitialRecordViewSet,
+    RatingViewSet
+)
 
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet)
 router.register(r"admins", AdminViewSet)
-router.register(r"doctors", DoctorViewSet)
+router.register(r"doctors", DoctorViewSet, basename="doctor")
 router.register(r"patients", PatientViewSet)
 router.register(r"specialties", SpecialtyViewSet)
 router.register(r"services", ServiceViewSet)
